a4d47143c71d9f158a4ee1edfaf87e8a62c5160d
Aakash Mathur <aakashrajmathur@gmail.com>
2016-12-18 18:31:55 -0600

    speed up reads, not write unless odds change

--- DobbyTheOddsElf/DobbyTheOddsElf/MainWindow.xaml.cs
+++ DobbyTheOddsElf/DobbyTheOddsElf/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Input;
 using System.Linq;
 using Awesomium.Core;
+using mshtml;
 
 namespace DobbyTheOddsElf
 {
@@ -15,10 +16,16 @@ namespace DobbyTheOddsElf
     /// </summary>
     public partial class MainWindow : Window
     {
-        const int TIMER_DURATION = 15000;
+        const int TIMER_DURATION = 1000;    //1second
         const int MINIMUM_LENGTH_OF_TEAM_NAME = 4;
 
+        string teamName1 = "";
+        string currentOddsTeam1 = "";
+        string teamName2 = "";
+        string currentOddsTeam2 = "";
+        List<OddsRecord> oddsRecords;
         Timer timer;
+        Timer timerReadTeamNames;
         SQLiteConnection sqlconn;
         public static string databaseName;
 
@@ -27,8 +34,14 @@ namespace DobbyTheOddsElf
             try
             {
                 InitializeComponent();
+
+                timerReadTeamNames = new Timer(1000);
+                timerReadTeamNames.Elapsed += new ElapsedEventHandler(GetTeamNames);
+
                 timer = new Timer(TIMER_DURATION);
                 timer.Elapsed += new ElapsedEventHandler(timerElapsed);
+
+                oddsRecords = new List<OddsRecord>();
 
                 string[] args = Environment.GetCommandLineArgs();
                 if (args.Count() == 1)
@@ -38,13 +51,14 @@ namespace DobbyTheOddsElf
                 else
                 {
                     NavigateMainWebBrowser(args[3]);
-                    textBoxTeam1.Text = args[1];
-                    textBoxTeam2.Text = args[2];
+
+                    timerReadTeamNames.Start();
+
                     databaseName = args[4];
                     WindowState = WindowState.Minimized; //Default Window state Minimized                    
-                    timer.Start();
+                    //timer.Start();
                 }
-                CreateSQLiteDatabase();
+                //CreateSQLiteDatabase();
 
             }
             catch (Exception ex) {
@@ -52,13 +66,70 @@ namespace DobbyTheOddsElf
             }
         }
 
-        private void CreateSQLiteDatabase()
+        private void GetTeamNames(object sender, ElapsedEventArgs e)
         {
-            sqlconn = new SQLiteConnection("Data Source=" + GetDatabaseLocation() + GetDatabaseName() + ".sqlite;Version=3;New=True;Compress=True;");
-            sqlconn.Open();
-            SQLiteCommand createQ = new SQLiteCommand("CREATE TABLE IF NOT EXISTS sportsLiveRates( DateTime TEXT, Team TEXT, Rate TEXT)", sqlconn);
-            createQ.ExecuteNonQuery();
-            sqlconn.Close();
+            try
+            {
+                Dispatcher.Invoke(new Action(() =>
+                {
+                    IHTMLElement couponHTMLElement = ((HTMLDocument)mainWebBrowser.Document).getElementById("coupon");
+                    if (couponHTMLElement != null)
+                    {
+                        string header = GetHeaderInCoupon(couponHTMLElement.innerHTML);
+
+                        string[] teamsTokens = header.Split(new string[] { "VS", "Vs", "vs", "vS", "@" }, System.StringSplitOptions.None);
+
+                        if (teamsTokens.Length == 2)
+                        {
+                            teamName1 = GetClensedString(teamsTokens[0]);
+                            teamName2 = GetClensedString(teamsTokens[1]);
+                            timer.Start();
+                            timerReadTeamNames.Stop();
+                        }
+                    }
+
+                }));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private string GetHeaderInCoupon(string webPageSource)
+        {
+            //Remove all new line characters: 
+            webPageSource = webPageSource.Replace("\n", "");
+
+            bool isInTag = false;
+            //List<string> processedWebSource = new List<string>();
+            string tempBuffer = "";
+            foreach (char c in webPageSource.ToCharArray())
+            {
+                if (c == '<')
+                {
+                    tempBuffer = tempBuffer.TrimStart();
+                    tempBuffer = tempBuffer.TrimEnd();
+                    if (tempBuffer.Length > 0)
+                    {
+                        return tempBuffer;
+                        //processedWebSource.Add(tempBuffer);
+                        //tempBuffer = "";
+                    }
+                    isInTag = true;
+                }
+                else if (c == '>')
+                {
+                    isInTag = false;
+                    continue;
+                }
+
+                if (!isInTag)
+                {
+                    tempBuffer += c;
+                }
+            }
+            return "";
         }
 
         private string GetDatabaseLocation()
@@ -75,7 +146,7 @@ namespace DobbyTheOddsElf
         {
             //Objective: Give Database a unique name, Convention: date time + processID: 
             if (databaseName == null)
-                return DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss");
+                return DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss_") + teamName1 + "_vs_" + teamName2;
             else
                 return databaseName;
         }
@@ -84,16 +155,70 @@ namespace DobbyTheOddsElf
         {
             try
             {
-                //MessageBox.Show("timer elapsed");
                 Dispatcher.Invoke(new Action(() =>
                 {
-                    ReportRates();
+                    string updatedOdds = GetClensedString(GetOdds(teamName1));
+                    if (currentOddsTeam1.CompareTo(updatedOdds) != 0)
+                    {
+                        listBoxQuerySubmitted.Items.Add("Odds updated for " + teamName1 + " to " + updatedOdds + " from " + currentOddsTeam1);
+                        currentOddsTeam1 = updatedOdds;
+                        oddsRecords.Add(new OddsRecord(teamName1, updatedOdds));
+                    }
+
+                    updatedOdds = GetClensedString(GetOdds(teamName2));
+                    if (currentOddsTeam2.CompareTo(updatedOdds) != 0)
+                    {
+                        listBoxQuerySubmitted.Items.Add("Odds updated for " + teamName2 + " to " + updatedOdds + " from " + currentOddsTeam2);
+                        currentOddsTeam2 = updatedOdds;
+                        oddsRecords.Add(new OddsRecord(teamName2, updatedOdds));
+                    }
                 }));
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
+        }
+
+        private void UpdateOdds(string teamName, string currentOdds)
+        {
+            string updatedOdds = GetClensedString(GetOdds(teamName));
+            if (currentOdds.CompareTo(updatedOdds) != 0)
+            {
+                listBoxQuerySubmitted.Items.Add("Odds updated for " + teamName + " to " + updatedOdds + " from " + currentOdds);
+                currentOdds = updatedOdds;
+                oddsRecords.Add(new OddsRecord(teamName, updatedOdds));                
+            }
+        }
+
+        private string GetOdds(string teamName)
+        {
+            
+            IHTMLElementCollection liCollection = ((HTMLDocument)(mainWebBrowser.Document)).getElementsByTagName("li");
+            foreach (IHTMLElement elem in liCollection)
+            {
+                if (elem.innerHTML != null)
+                {
+                    if (elem.innerHTML.ToUpper().Contains("MONEYLINE"))
+                    {
+                        List<string> processedWebSource = GetProcessedWebSource(elem.innerHTML);
+                        
+
+                        for(int i = 0; i < processedWebSource.Count; i++)
+                        {
+                            if(processedWebSource[i].TrimStart().TrimEnd().ToUpper().CompareTo(teamName.TrimStart().TrimEnd().ToUpper()) == 0)
+                            {
+                                if(processedWebSource.Count >= i + 1)
+                                {
+                                    return processedWebSource[i + 1];
+                                }
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+            return "";
         }
 
         private void ReportRates()
@@ -279,11 +404,35 @@ namespace DobbyTheOddsElf
             return dom.body.innerHTML;
         }
 
+        private string GetClensedString(string input, string replacementText = "")
+        {
+            return System.Text.RegularExpressions.Regex.Replace(input.TrimStart().TrimEnd(), @"[^0-9a-zA-Z\-]+", replacementText);
+        }
+
         private void monitorButton_Click(object sender, RoutedEventArgs e)
         {
             if(monitorButton.Content.ToString() == "Start")
             {
-                timer.Start();
+                //timer.Start();
+
+                IHTMLElement couponHTMLElement = ((HTMLDocument)mainWebBrowser.Document).getElementById("coupon");
+                if (couponHTMLElement != null)
+                {
+                    string header = GetHeaderInCoupon(couponHTMLElement.innerHTML);
+
+                    string[] teamsTokens = header.Split(new string[] { "VS", "Vs", "vs", "vS", "@" }, System.StringSplitOptions.None);
+
+                    if (teamsTokens.Length == 2)
+                    {
+                        teamName1 = teamsTokens[0].TrimStart().TrimEnd();
+                        textBoxTeam1.Text = teamName1;
+                        teamName2 = teamsTokens[1].TrimStart().TrimEnd();
+                        textBoxTeam2.Text = teamName2;
+                        timer.Start();
+                        timerReadTeamNames.Stop();
+                    }
+                }
+
                 monitorButton.Content = "Stop";
             }else
             {
@@ -291,5 +440,31 @@ namespace DobbyTheOddsElf
                 monitorButton.Content = "Start";
             }
         }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            sqlconn = new SQLiteConnection("Data Source=" + GetDatabaseLocation() + GetDatabaseName() + ".sqlite;Version=3;New=True;Compress=True;");
+            sqlconn.Open();
+            SQLiteCommand createQ = new SQLiteCommand("CREATE TABLE IF NOT EXISTS sportsLiveRates( DateTime TEXT, Team TEXT, Rate TEXT)", sqlconn);
+            createQ.ExecuteNonQuery();
+            
+            foreach (OddsRecord oddsRecord in oddsRecords)
+            {
+                string dateTime = oddsRecord.dateTime.ToString("yyyy/MM/dd HH:mm:ss");
+                string insertCommand = @"INSERT INTO sportsLiveRates (DateTime, Team, Rate) VALUES ("" " + dateTime + @" "","" " + oddsRecord.teamName + @" "",""" + oddsRecord.odds + @""");";
+                SQLiteCommand insertSQL = new SQLiteCommand(insertCommand, sqlconn);
+                try
+                {
+                    insertSQL.ExecuteNonQuery();
+                    //listBoxQuerySubmitted.Items.Add(insertSQL.CommandText.ToString());
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+            sqlconn.Close();
+        }
+        
     }
 }
---
+++ DobbyTheOddsElf/DobbyTheOddsElf/OddsRecord.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DobbyTheOddsElf
+{
+    class OddsRecord
+    {
+        public string teamName;
+        public string odds;
+        public DateTime dateTime; 
+
+        public OddsRecord(string teamName, string odds)
+        {
+            this.teamName = teamName;
+            this.odds = odds;
+            this.dateTime = DateTime.Now;
+        }
+    }
+}
